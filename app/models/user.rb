37fac300cdd9b2886868972a7f17ecfa89c9526f
Justin Rosendahl <jr4171@cerner.com>
2014-11-14 08:41:53 -0600

    end of 7.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,19 +13,29 @@ class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
 
-#  validates_presence_of :name
-#  validates_presence_of :email
+
+
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
+#  validates_presence_of :name
   validates :name, :presence => true,
                    :length => { :maximum => 50 }
-
+#  validates_presence_of :email
   validates :email, :presence => true,
             :format => { :with => email_regex },
             :uniqueness => { :case_sensitive => false}
-
+# validates_presence_of :password and :password_confirmation
   validates :password, :presence => true,
             :confirmation => true,
             :length => { :within => 6..40 }
+  before_save :encrypt_password
+
+  private
+
+    def encrypt_password
+      self.encrypted_password = encrypt password
+    end
+    def encrypt string
+      string # Only a temporary impl
+    end
 end
